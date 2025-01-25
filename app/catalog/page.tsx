fbd77c58c9f9eb3a53789333d5d65e1f4f5234f3
AdamSdes <111901788+AdamSdes@users.noreply.github.com>
2025-01-25 23:38:21 +0300

    Revert "fix 2.0"

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,15 +1,5 @@
 import CatalogPage from '@/components/Catalog/CatalogPage';
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import Support from "@/components/Support/Support";
-export default function Page() {
 
-     return (
-    <>
-    <Support />
-      <Header />
-     <CatalogPage />
-    <Footer />
-    </>
-  );
+export default function Page() {
+    return <CatalogPage />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,27 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
-import { useState, useEffect } from 'react';
 import Support from "@/components/Support/Support";
 import Header from "@/components/Header/Header";
-import Image from "next/image";
+import Carousel from "@/components/Carousel/Carousel";
+import TopProduct from "@/components/TopProduct/TopProduct";
+import FAQWidget from "@/components/FAQ/FAQWidget";
+import Coments from "@/components/Coments/Coments";
 import Footer from "@/components/Footer/Footer";
-import Link from "next/link";
+
+import { VelocityScroll } from "@/components/ui/scroll-based-velocity";
+
 
 export default function Home() {
-
-
   return (
     <>
       <Support />
       <Header />
-    <Footer />
+      <Carousel />
+      <VelocityScroll>MEMO GAMES</VelocityScroll>
+      <TopProduct />
+      <Coments />
+      <FAQWidget />
+      <Footer />
     </>
   );
 }
